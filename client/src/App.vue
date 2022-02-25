d48121f172188cb8777af63399d910efb5c77216
xone46 <achraflahcen46@gmail.com>
2022-02-26 00:32:10 +0100

    add landing and gthconsult

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,120 +1,110 @@
 <template>
   <div id="app">
+    
+
+
+    <!-- Start Header General -->
+
     <div id="header" class="header">
       <div class="container">
-        <img alt="" src="@/assets/logo.png" class="logo">
-        <ul class="main-nav">
-          <li>
-            <router-link to="/">
-              <a href="">La société</a>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/nos">
-               <a href="">Nos métiers</a>
-             </router-link>
-          </li>
-          <li>
-            <router-link to="/car">
-               <a href="">Carrières</a>
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/client">
-              <a href="">Espace Clients</a>
-            </router-link>
-          </li>
-        </ul>
+
+         <!--  start top Header -->
+        <div class="top-header">
+          <p>GTH consult Bureau de contrôle agréé par l’état</p>
+          <p> <i class="fa-solid fa-location-dot"></i> 3<sup>ème</sup> Étage Appartement N<sup>o</sup> 09, Mohammédia, Maroc</p>
+          <p> <i class="fa-solid fa-phone"></i> +212 5 23 27 27 74 <i class="fa-solid fa-print"></i> +212 5 23 27 29 79</p>
+        </div>
+        <!-- end top header -->
+  
+
+
+        <!-- start bootom Header -->
+        <div class="bottom-header">
+           <!--  start logo -->
+            <img alt="" src="@/assets/logo.png" class="logo">
+           <!-- end logo -->
+
+           <!-- Start Navbar -->
+            <ul class="main-nav">
+              <li>
+                  <a href="#gthconsult">GTH consult</a>
+              </li>
+              <li>
+                  <a href="#prestations">Nos prestations</a>
+              </li>
+              <li>
+                  <a href="#solutions">Nos solutions</a>
+              </li>
+              <li>
+                <router-link to="/client">
+                  <a href="">Espace Clients</a>
+                </router-link>
+              </li>
+              <li>
+                  <a href="#car">Carrières</a>
+              </li>
+              <li>
+                  <a href="#contacter">Nous contacter</a>
+              </li>
+            </ul>
+            <!-- End Navbar -->
+
+        </div>
+        <!-- end bootom Header -->
       </div>
     </div>
+
+    <!-- End Header General -->
 
     <router-view/>
 
-    <div id="footer" class="footer">
-      <div class="container">
-        <div class="wave">
-          <img src="@/assets/wave.png" alt="">
-        </div>
-        <div class="social">
-          <ul>
-              <li><a href=""><i class="fa-brands fa-linkedin"></i></a></li>
-              <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
-              <li><a href=""><i class="fa-brands fa-linkedin"></i></a></li>
-              <li><a href=""><i class="fa-brands fa-twitter"></i></a></li>
-          </ul>
-        </div>
-        <div class="info">
-          <div class="col">
-            <img src="@/assets/logo.png" alt="">
-            <p>blablablav blavvblabla bla blavbla v blablabla blabla vbla bla blabla blabla vv vbla blabla bla bla bla bla blablabla</p>
-          </div>
-          <div class="col">
-            <h3>Office</h3>
-            <p><i class="fa-solid fa-map-location"></i> Adresse : Bd Mohammed VI, Imm FB08, n°9, 28820</p>
-            <p><i class="fa-solid fa-at"></i> Email : service@gthconsult.com</p>
-            <p><i class="fa-solid fa-phone"></i> Telephone : +2125232-72774</p>
-            <p><i class="fa-solid fa-browser"></i> Website : www.gthconsult.com </p>
-          </div>
-          <div class="col">
-            <h3>Liens</h3>
-            <p><a href="#"> société</a></p>
-            <p> <a href="#">Nos métiers</a></p>
-            <p> <a href="#">Carrières</a></p>
-            <p> <a href="#">Espace Clients</a></p>
-          </div>
-          <div class="col">
-            <h3>Découvrir les nouvelle</h3>
-            <form> 
-              <a href="#"><i class="fa-solid fa-envelope"></i></a>
-              <input type="text" placeholder="Email adresse" required>
-              <button type="submit"><i class="fa-solid fa-arrow-right"></i></button>
-            </form>
-          </div>
-        </div>
-      </div>
-      
-    </div>
-
+   
   </div>
 </template>
 
 <style scoped>
-/* start header */
-#header {
-  background-color: white;
-  -webkit-box-shadow: 0 0 10px 0 #ddd;
-  -moz-box-shadow: 0 0 10px 0 #ddd;
-  box-shadow: 0 0 10px 0 #ddd;
+.header{
+  position: fixed;
+  width: 100%;
+  z-index: 2;
 }
 
-#header .container {
+.header .container {
   padding-left: 15px;
   padding-right: 15px;
   margin-left: auto;
   margin-right: auto;
-  background-color: white;
+}
+
+.header .container .top-header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
+}
+.header .container .top-header p {
+  color :#cfcbcb;
+  font-size: 13px;
+}
+
+.header .container .bottom-header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  flex-wrap: wrap;
-  position: relative;
+  background-color:white;
 }
-
-#header .logo {
-  height: 72px;
+.header .container .bottom-header .logo {
+  width: 49px;
   display: flex;
   justify-content: center;
   align-items: center;
-} 
+}
 
+.header .container .bottom-header .main-nav {
+  display: flex;
+}
+.header .container .bottom-header .main-nav > li > a  {
 
-#header .main-nav {
- display: flex;
-} 
-
-
-
-#header .main-nav > li > a {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -124,10 +114,11 @@
   color : var(--alt-main-color);
   transition: var(--main-transition);
   overflow: hidden;
- 
-} 
 
-#header .main-nav > li > a::before {
+}
+
+.header .container .bottom-header .main-nav > li > a::before  {
+
   content :"";
   position: absolute;
   background-color: var(--main-color);
@@ -137,9 +128,10 @@
   left :-100%;
   transition: var(--main-transition);
 
-} 
+}
 
-#header .main-nav > li > a::after {
+.header .container .bottom-header .main-nav > li > a::after  {
+
   content :"";
   position: absolute;
   background-color: var(--main-color);
@@ -149,124 +141,21 @@
   left :-100%;
   transition: var(--main-transition);
 
-} 
+}
 
+.header .container .bottom-header .main-nav > li > a:hover  {
 
-#header .main-nav > li > a:hover {
   color: var(--main-color);
   background-color: #fafafa;
-} 
 
-#header .main-nav > li > a:hover::before {
-left :0;
 }
 
-#header .main-nav > li > a:hover::after {
-left :0;
-}
-/* end Header */
-
-
-/* start footer */
-
-#footer {
-margin-top: 10px;  
+.header .container .bottom-header .main-nav > li > a:hover::before  {
+ left: 0;
 }
 
-#footer .container {
-  position: relative;
+.header .container .bottom-header .main-nav > li > a:hover::after  {
+ left: 0;
 }
-#footer .container .wave {
-  overflow: hidden;
-}
-#footer .container .social {
-  background: var(--alt-main-color);
-  height: 80px;
-}
-
-#footer .container .social ul {
-    display: flex;
-    justify-content: center;
-    align-items: center;  
-    height: 80px;
-}
-#footer .container .social ul li {
-  margin-top:20 ;
-  padding: 20px;
-}
-#footer .container .social ul li:hover {
- transform: translateY(-10px);
- transition: var(--main-transition);
-}
-
-#footer .container .social ul li a {
-color :white;
-font-size: 2em;
-}
-
-#footer .container .info {
-  background: var(--alt-main-color);
-  height: 200px;
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  justify-content: space-between;
-}
-
-#footer .container .info .col {
- flex-basis: 25%;
- padding: 10px;
-}
-
-#footer .container .info .col img {
-    width:50px;
-} 
-
-#footer .container .info .col p {
-color: white;
-font-size: 10px;
-}
-
-#footer .container .info .col h3 {
-color: white;
-}
-#footer .container .info .col a {
-color: white;
-}
-
-form {
-  padding-bottom: 15px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-bottom: 1px solid #ccc;
-  margin-bottom: 50px;
-}
-form a{
-  color:white;
-}
-form input{
-  color:white;
-  background: transparent;
-  color:white;
-  border: 0;
-  outline: none;
-}
-
-form button {
-  background: transparent;
-  border: 0;
-  outline: none;
-  cursor: pointer;
-}
-form button .fa-arrow-right{
-  color:white;
-}
-
-
- 
-
-
-/* end footer */
 
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,198 +1,322 @@
 <template>
-  <div class="home">
-    <div id="landing">
+
+
+  <div class="landing">
       <div class="container">
-        <div class="slideshow-item">
-          <img src="@/assets/1.jpg" alt="">
-          <div class="slideshow-item-text">
-            <h5>New Yourk City</h5>
+
+        <div class="image">
+            <div class="text">
+              <div class="content">
+                <h1>GTH Consult <br>Pour le respect de vos obligations réglementaires et volontaires</h1>
+                <p>Vérifications Réglementaires ou volontaires des installations techniques et des risques au travail</p>
+              </div>
+            </div>
+            <i class="fas fa-angle-left change-background fa-2x"></i>
+            <i class="fas fa-angle-right change-background fa-2x"></i>
+            <ul class="bullets">
+              <li></li>
+              <li class="active"></li>
+              <li></li>
+            </ul>
+        </div>
+      </div>
+
+      <div class="gthconsult" id="gthconsult">
+        <h1>Notre objectif est de répondre à vos obligations réglementaires et volontaires</h1>
+        <div class="blogs">
+          <div class="blogOne">
+            <div class="card">
+              <img src="../assets/risk.png"  class="risk" alt="risk">
+                <h1>Maîtrise des risques</h1>
+                <p> Accompagner les chefs d’établissements et les
+                    exploitants pour répondre à leurs obligations
+                    réglementaires ou volontaires en matière de gestion
+                    des risques professionnels, protection des personnes
+                    et des biens ainsi que de l’environnement.
+                </p>
+            </div>
+          </div>
+          <div class="blogTow">
+            <div class="card">
+                <img src="../assets/know.png"  class="know" alt="know">
+                <h1>Partage du savoir</h1>
+                <p>
+                  Notre capital est notre savoir-faire, nos compétences
+                  et le respect de la réglementation. Notre vocation de
+                  tierce partie indépendante nous impose le partage de
+                  ces compétences et de ces savoirs entre nos
+                  collaborateurs, avec nos clients et les institutions.
+                </p>
+            </div>
+          </div>
+          <div class="blogTree">
+            <div class="card">
+                <img src="../assets/quality.png"  class="quality" alt="quality">
+                <h1>Interlocuteur privilégié</h1>
+                <p>
+                  Mettre à la disposition de notre client un interlocuteur
+                  unique habilité à prendre toute décisions pour
+                  garantir la qualité de nos prestations, le respect de
+                  nos engagements et notre disponibilité pour nos
+                  clients.
+
+                </p>
+            </div>
+          </div>
+          <div class="blogFour">
+            <div class="card">
+                <img src="../assets/help.png"  class="help" alt="help">
+                <h1>Nos valeurs</h1>
+                <ul>
+                  <li> • Qualité de la prestation</li>
+                  <li> • Veille réglementaire</li>
+                  <li> • Respect de la réglementation</li>
+                  <li> • Répondre aux besoins clients</li>
+                  <li> • Sécurité de nos collaborateurs</li>
+                  <li> • Innovation et performance</li>
+                </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-
-    <div id="activites">
-      <div class="container">
-        <div class="one">
-          <h1>Nos métiers</h1>
-          <p>Découvrez nos services dans les secteurs de la construction, de l'immobilier, du tertiaire et de l'industrie.</p>
-        </div>
-        <div class="tow">
-          <img src="@/assets/2.jpg" alt="">
-        </div>
-        <div class="tree">
-          <img src="@/assets/3.jpg" alt="">
-        </div>
-        <div class="four">
-          <img src="@/assets/4.jpg" alt="">
-        </div>
-        <div class="five">
-          <img src="@/assets/5.jpg" alt="">
-        </div>
-        <div class="six">
-          <img src="@/assets/6.jpg" alt="">
-        </div>
-      </div>
-    </div>
-
-
   </div>
+
+
+
 </template>
 
 <style scoped>
-.home {
-  height: 100%;
-}
-/* start linding */
-#landing {
-  margin-top: 5px;
+
+/* Start Landing */
+.landing{
+  width: 100%;
 }
 
-#landing .container {
+.landing .container {
   padding-left: 15px;
   padding-right: 15px;
   margin-left: auto;
   margin-right: auto;
 }
 
-#landing .container .slideshow-item {
-  height: 400px;
+.landing .container .image {
+  min-height: 100vh;
+  background-image: url('../assets/1.jpg');
+  background-size: cover;
   position: relative;
 }
 
-#landing .container .slideshow-item  img{
-  width: 100%;
-  height: 400px;
+.landing .container .image .text {
   position: absolute;
+  left:0;
+  top: 30%;
+  transform: translateY(50%);
+  background-color: #31438fa1;
+  color: white;
+  padding-left: 60px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 20px;
+  border-radius: 0px 100px 0px 0px;
+  border-bottom: solid 5px #f7000094;
+
 }
 
+.landing .container .image .text .content h1 {
 
+  color: white;
+  font-size: 30px;
+  line-height: 1;
+
+}
+
+.landing .container .image .text .content p {
+  text-align: center;
+  font-size: 18px;
+}
+
+.landing .container .image .change-background {
+  color:#ddd;
+  position: absolute;
+  top: 60%;
+  transform: translateY(60);
+}
+
+.landing .container .image .fa-angle-left {
+  left: 30px;
+}
+
+.landing .container .image .fa-angle-right {
+  right: 30px;
+}
+
+.landing .container .image .fa-angle-right {
+  right: 30px;
+}
+
+.landing .container .image .bullets {
+ 
+    position: absolute;
+    left:50%;
+    transform: translateY(-50%);
+    bottom: 30px;
+    display: flex;
+
+ }
+
+.landing .container .image .bullets li {
+ 
+    width:20px;
+    height: 20px;
+    border: 1px solid white;
+    border-radius: 50%;
+    margin-right: 10px;
+    outline: 1px solid #31438fa1 ;
+
+ }
+
+ .landing .container .image .bullets li:hover {
+ 
+    border: 1px solid #31438fa1;
+    background-color: #31438fa1;
+
+ }
 
 /* end landing */
 
-
-/* start activites */
-
-#activites {
-  margin-top: 30px;
+/* start gthconsult */
+.gthconsult {
+    height: 600px;
 }
 
-#activites .container {
-  padding-left: 15px;
-  padding-right: 15px;
-  margin-left: auto;
-  margin-right: auto;
-  position: relative;
-  display:flex;
+.blogs {
+  margin: 0;
+  display: flex; 
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+
+}
+
+.blogOne , .blogTow , .blogTree , .blogFour  {
+  width: calc(100% / 2);
+  height: 300px;
+  display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: row;
-  flex-wrap: wrap;
-}
-#activites .container > .one, .tow, .tree, .four, .five, .six {
-  width :calc((100%-30px) / 3);
-  height: 300px;
-  overflow: hidden;
 }
 
+.blogOne .card {
+  height: 80%;
+  width: 60%;
+  background-color: white;
+  border: 0px;
+  box-shadow: 3px 8px #374173;
+  position: relative;
+  border-radius: 20px;
+}
+.blogTow .card {
+  height: 80%;
+  width: 60%;
+  background-color:white;
+  border: 0px;
+  box-shadow: 3px 8px #374173;
+  position: relative;
+  border-radius: 20px;
 
 
-#activites .container > .one > h1 {
-  margin-top: 20px;
-  color :var(--alt-main-color);
-  margin-left: 10px;
+}
+.blogTree .card {
+  height: 80%;
+  width: 60%;
+  background-color: white;
+  border: 0px;
+  box-shadow: 3px 8px #374173;
+  position: relative;
+  border-radius: 20px;
+
+
+}
+.blogFour .card {
+  height: 80%;
+  width: 60%;
+  background-color: white;
+  border: 0px;
+  box-shadow: 3px 8px #374173;
+  position: relative;
+  border-radius: 20px;
+
+
 }
 
-#activites .container > .one > p {
-    font-size: 15px;
-    margin-top: 20px;
-    margin-left: 10px;
-    width: 90%;
-    line-height: 25px;
+.risk , .know , .quality , .help {
+    
+    position: absolute;
+    left: -20px;
+    height: auto;
+    max-width: 25%;
+}
+
+.gthconsult h1 {
+  color: #374173;
+  text-align: center;
+  margin-top: 25px;
+  margin-bottom: 10px;
+  font-weight: 800;
+  font-size: 20px;
+  letter-spacing: normal;
+}
+.card p {
+    text-align: center;
+    padding-top: 28px;
+    letter-spacing: 1px;
+}
+.card ul {
+   margin-left: 65px;
+   padding-top: 28px;
 }
 
 
-#activites .container > .one > img {
 
-    display: block;
-    max-width: 100%;
-    max-height: 100%;
-}
 
-/* end activites */
+/* end gthconsult */
+
+
+
+
+
+
 
 </style>
 
 <script>
-// import ClientService from "../ClientService";
-// import RapportService from "../RapportService";
-// export default {
-//   name: "Home",
-//   data() {
-//     return {
-//       clients: [],
-//       error: "",
-//       file : null,
-//       titre : null,
-//       description : null,
-//       clientId : '',
-//       filename : ''
+// const arr = [1,2,3]
 
-//     };
-//   },
-//   components: {},
-//   methods: {
+// function handelSlider() {
 
-//     getPdf() {
-//         RapportService.getRapport(this.filename)
-//           .then((data) => {
-//             console.log(data)
-//           })
-//           .catch((error) => {
-//             console.error(`HTTP error: ${error.name} => ${error.message}`);
-//             throw "fail request at: GET /refreshtime";
-//           })
-//     },
-
-//     selectFile() {
-
-//          this.file = this.$refs.file.files[0];
-         
-//     },
-//     sendFile() {
-//         RapportService.insertRapport(this.file, this.clientId)
-//           .then((data) => {
-//             console.log(data)
-//           })
-//           .catch((error) => {
-//             console.error(`HTTP error: ${error.name} => ${error.message}`);
-//             throw "fail request at: GET /refreshtime";
-//           });
-//     },
-//     updateFile() {
-//         RapportService.updateRapport(this.file, this.clientId)
-//           .then((data) => {
-//             console.log(data)
-//           })
-//           .catch((error) => {
-//             console.error(`HTTP error: ${error.name} => ${error.message}`);
-//             throw "fail request at: GET /refreshtime";
-//           });
+// const value = arr[(Math.random() * arr.length) | 0];
+ 
+//     setTimeout(()=> {
+//       handelSlider()} ,10000)
 //     }
-//   },
 
+//   handelSlider()
 
+export default {
+  name: "Home",
+  data() {
+    return {
+    };
+  },
+  methods : {
 
-//   async mounted() {
-//     ClientService.getClients()
-//       .then((data) => {
-//         this.clients = data.client;
-//       })
-//       .catch((error) => {
-//         console.error(`HTTP error: ${error.name} => ${error.message}`);
-//         throw "fail request at: GET /refreshtime";
-//       });
-//   },
-// };
+  },
+
+  computed: {
+
+  }
+
+}
+
 </script>
