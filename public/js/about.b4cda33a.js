fd9588d060c147499de0d3674d987f77c279ec6d
xone46 <achraflahcen46@gmail.com>
2022-03-01 23:23:23 +0100

    test heroku

---
+++ public/js/about.b4cda33a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var s=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"about"},[e("font-awesome-icon",{attrs:{icon:"spinner"}})],1)},a=[],o=e("2877"),c={},i=Object(o["a"])(c,s,a,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=about.b4cda33a.js.map
